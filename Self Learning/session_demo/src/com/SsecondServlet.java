32a7f9337027a63ed1d20a17facc1c1dfa895b10
Paul Aguilar <pauleaguilar@gmail.com>
2021-02-24 23:18:12 -0600

    added session management project

---
+++ Self Learning/session_demo/src/com/SsecondServlet.java
@@ -0,0 +1,50 @@
+package com;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dto.User;
+
+/**
+ * Servlet implementation class SsecondServlet
+ */
+public class SsecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SsecondServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+		HttpSession session = request.getSession(false);
+		
+		
+		if(session !=null) {
+			
+			User u = (User) session.getAttribute("user");
+			u.setEmail(request.getParameter("email"));
+			u.setAge( Integer.parseInt(request.getParameter("age")));
+			session.setAttribute("user", u);
+			response.sendRedirect("three.html");
+			
+		}
+		
+		
+		
+	}
+
+}
---
+++ Self Learning/session_demo/src/com/SummaryServlet.java
@@ -0,0 +1,53 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dto.User;
+
+/**
+ * Servlet implementation class SummaryServlet
+ */
+public class SummaryServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SummaryServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession(false);
+		
+		
+		if(session !=null) {
+			
+			User u = (User) session.getAttribute("user");
+			u.setCity(request.getParameter("city"));
+			u.setContact( Long.parseLong(request.getParameter("contact")));
+			
+			PrintWriter out = response.getWriter();
+			
+			out.println("<h2> Hello " + u.getEmail() + "</h2>");
+			out.println("<h3> Details .. " + u + "</h3>");
+			out.println("<h4> Session Id is " + session.getId() + " </h4>");
+			out.println("<h4> Session created at " + session.getCreationTime() + "</h4>");
+			session.invalidate();
+		}
+	}
+    
+    
+    
+
+}
